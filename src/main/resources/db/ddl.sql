7fcaf782f2b3e8be75b5a3eb7a4add2264de7e96
Hsu <emily.hsu@wpgholdings.com>
2025-07-12 22:31:11 +0800

    檔案修改

--- src/main/resources/db/ddl.sql
+++ src/main/resources/db/ddl.sql
@@ -7,5 +7,6 @@ CREATE TABLE vote_record (
     id INT AUTO_INCREMENT PRIMARY KEY,
     voter_name VARCHAR(100) NOT NULL,
     vote_item_id INT NOT NULL,
+    vote_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (vote_item_id) REFERENCES vote_item(id)
 );
